ad7255b61d09befa86c1d19082c4ca22fd3c906f
Manax <manax.punk182@gmail.com>
2022-02-06 03:39:03 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import Section from './Components/Section'
+import HeroSection from './Components/HeroSection'
+import Navbar from './Components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const genreIncrement = 4
+    const [genres, setGenres] = useState(null);
+    const [limit, setLimit] = useState(genreIncrement);
+
+    const fetchData = async () => {
+        const response = await fetch("/.netlify/functions/getGenres", {
+            method: "POST",
+            body: limit
+        })
+        const responseBody = await response.json()
+        setGenres(responseBody.data.reference_list.values)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [, limit])
+
+
+    return (
+        <>
+            <Navbar/>
+            <HeroSection/> {
+            genres && (
+                <div className="container">
+                    {
+                    Object.values(genres).map((genre, index) => (
+                        <Section key={index}
+                            genre={
+                                genre.value
+                            }/>
+                    ))
+                } </div>
+            )
+        }
+            <div className="page-end"
+                onMouseEnter={
+                    () => {
+                        setLimit(limit + genreIncrement)
+                    }
+            }></div>
+    </>
+    );
 }
 
 export default App;
